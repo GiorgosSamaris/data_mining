b322d7868c70a3a50795fa9c802d92f21efc6c2e
ptriantafy <pantrian04@gmail.com>
2024-03-28 14:13:43 +0200

    forgot to add call to drop_outliers

--- src/main.py
+++ src/main.py
@@ -12,7 +12,8 @@ for file in os.listdir(constants.CSV_PATH):
     if temp_df.isnull():
         continue
     temp_df = csv_handler.drop_nonuniform_columns(temp_df)
-     #Check for outliers by calculating IQR for each column
+    #Check for outliers by calculating IQR for each column
+    temp_df = csv_handler.drop_outliers(temp_df)
     #Data bining
     # homogeneous_df= pd.concat([homogeneous_df, temp_df])
 
