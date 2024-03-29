fd81193ef13f8e0b03cc22b38efc25d6ff11894f
ptriantafy <pantrian04@gmail.com>
2024-03-29 10:42:15 +0200

    converted file paths relative to constants.py dir

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,4 @@
+import os
 # only declare constants here
-CSV_PATH = "../harth/harth/"
-TXT_OUTPUT_PATH = "./output.txt"
+CSV_PATH =  os.path.join(os.path.dirname(__file__), "../harth/harth/")
+TXT_OUTPUT_PATH = os.path.join(os.path.dirname(__file__), "output.txt")
