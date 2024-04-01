f6e58dc9712c594f4976ffffe94612593e5bca28
ptriantafy <pantrian04@gmail.com>
2024-04-01 10:50:32 +0300

    correctly added subject_id column and renamed vars for readability

--- src/main.py
+++ src/main.py
@@ -5,29 +5,18 @@ import constants
 import csv_handler
 homogeneous_df = pd.DataFrame()
 
-
-
 for file in os.listdir(constants.CSV_PATH):
     temp_df = csv_handler.read_csv(file)
-    
     if temp_df is None:
         continue
-  
     temp_df = csv_handler.drop_nonuniform_columns(temp_df)
     #Check for outliers by calculating IQR for each column
-    temp_df = csv_handler.drop_outliers(temp_df)
-
-    #get subject id
-    s_name = file.split('.')[0]
-    s_id = constants.subject_id[s_name]
-
-    #Add id of subject as an extra column
-    temp_df.insert(9, "subject_id", s_id)
-
-    #merge the dataframes
+    # temp_df = csv_handler.drop_outliers(temp_df)
+    subject_name = file.split('.')[0]
+    subject_id = constants.subject_id[subject_name]
+    temp_df["subject_id"] = subject_id
+    print(temp_df.head(1))
     homogeneous_df= pd.concat([homogeneous_df, temp_df])
-    
-    
 
 #remove index columns from csv 21 & 15
-print(homogeneous_df)
+print(homogeneous_df.head(0))
