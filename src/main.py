7cbe90d96bf421f1705043fd627ae777cb963b7d
ptriantafy <pantrian04@gmail.com>
2024-04-08 12:13:03 +0300

    conflict rename

--- src/main.py
+++ src/main.py
@@ -7,13 +7,13 @@ from utilities import Preprocessing as preproc
 import matplotlib.pyplot as plt
 import datetime
 
-def read_data(folder_path = ".",preproc = False) -> pd.DataFrame:
+def read_data(preprocess = False) -> pd.DataFrame:
     homogeneous_df = pd.DataFrame()
     for file in os.listdir(constants.CSV_PATH):
         temp_df = csv.read_csv(file)
         if temp_df is None:
             continue
-        if(preproc):
+        if(preprocess):
             temp_df = preproc.drop_nonuniform_columns(temp_df)
             temp_df = preproc.add_subject_id(temp_df, file, 8)
         homogeneous_df= pd.concat([homogeneous_df, temp_df])
@@ -27,13 +27,13 @@ def main():
     df = preproc.add_subject_id(data_frame=df, file_name="S006.csv", column_pos=8)
     print(df.size)
     print(df.head(1))
-    df_windowed = preproc.window_average(df_input=df, window_size=200)
+    df_windowed = preproc.window_data(df_input=df, window_size=200)
     print(df_windowed.size)
     print(df_windowed.head(10))
     plotter.activity_histogram(df_windowed)
     plotter.activity_histogram(df)
     if(constants.PROCESS):
-        merged_df = read_data(constants.CSV_PATH, preproc=False)
+        merged_df = read_data(preprocess=False)
         if not os.path.exists(constants.PROCESSED_CSV_PATH):
             print("process folder does not exist, creating...")
             # If it doesn't exist, create it
@@ -44,7 +44,6 @@ def main():
         merged_df = pd.read_csv(constants.PROCESSED_CSV_PATH + "proc_merged.csv")
         plotter.plot_gyro(merged_df)
         # back_sensor_df, thigh_sensor_df = preproc.separate_sensors(merged_df)
-        # plot_data = back_sensor_df.loc[:,["timestamp","back_x"]]
 
 if __name__ == "__main__":
     main()
