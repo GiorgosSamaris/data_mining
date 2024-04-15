f16c23edec9949a352bf54f7e161f44b9af3f431
ptriantafy <pantrian04@gmail.com>
2024-04-15 11:50:15 +0300

    fixxed train_test_split error and plotted conf matrix

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ from sklearn import naive_bayes as nb
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import numpy as np
 def read_data(file_path = ".") -> dict[pd.DataFrame]:
     dataframes = {}
@@ -37,20 +38,23 @@ def get_metrics(model, X_train, X_test, y_train, y_test):
     model.fit(X_train, y_train)
     # Get Predictions
     y_preds = model.predict(X_test)
+    cm = confusion_matrix(y_test, y_preds, labels=model.classes_)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
+    disp.plot()
+    plt.show()
     # Evaluate the model and append its score to model_scores
     #return model.score(X_test, y_test)
     metric_dict = {
     'accuracy_score' : accuracy_score(y_test, y_preds),
     'precision_score' : precision_score(y_test, y_preds, average='weighted', labels=np.unique(y_preds)),
-    'recall_score' : recall_score(y_test, y_preds),
-    'f1_score' : f1_score(y_test, y_preds),
-    'roc_auc_score' : roc_auc_score(y_test, y_preds),
+    'recall_score' : recall_score(y_test, y_preds, average='weighted'),
+    'f1_score' : f1_score(y_test, y_preds, average='weighted')
     }
     return metric_dict
 
 
 def main():
-    dataframes_dict = read_data(constants.PROCESSED_CSV_PATH)
+    dataframes_dict = read_data(constants.CSV_PATH)
     homogeneous_df = pd.DataFrame()
     for key in dataframes_dict:
         df = dataframes_dict[key]
@@ -67,12 +71,11 @@ def main():
         if constants.OPTIONS & 32 == constants.DROP_NON_UNIFORM_COLUMNS:
             df = preproc.drop_nonuniform_columns(df)
         if constants.OPTIONS & 64 == constants.MERGE:
-            homogeneous_df = pd.concat([homogeneous_df,df])
-    
-    X = homogeneous_df[['thigh_x', 'back_x']]
-    Y = homogeneous_df[['label']]
-    X_train, X_test, y_train, y_test = train_test_split(np.transpose(X.to_numpy()),np.transpose(Y.to_numpy()),test_size=0.2)
-    
+            homogeneous_df = pd.concat([homogeneous_df, df])
+
+    X = homogeneous_df[["thigh_x", 'back_x', 'thigh_y', 'back_y', 'thigh_z', 'back_z']]
+    Y = homogeneous_df[["label"]]
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25)
     model = GaussianNB()
     print(get_metrics(model,X_train,X_test,y_train,y_test))
 if __name__ == "__main__":
